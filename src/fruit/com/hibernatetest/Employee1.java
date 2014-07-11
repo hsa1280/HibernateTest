54c868e8e5536a878ddc4252394ddfdad144343e
SHIAN HUANG <shian.huang@ftlinc.com>
2014-07-11 13:44:35 -0500

    changed old employee class to employee1

---
+++ src/fruit/com/hibernatetest/Employee1.java
@@ -0,0 +1,54 @@
+package fruit.com.hibernatetest;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	
+	public Employee(){
+		
+	}
+	
+	public Employee( String fname, String lname, int salary ) {
+		
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastNmae) {
+		this.lastName = lastNmae;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
